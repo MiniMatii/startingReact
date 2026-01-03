0b89fe6ef045a36800a0448227f16de88a7f2549
MiniMati <matiasefracchia@hottmail.com>
2026-01-03 13:23:11 -0300

    03012026 Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+
+import { CORE_CONCEPTS } from './data'; 
+
+
+
+
+
+//First custom component in its own file + Randomized description moved to components/header.jsx
+
+//Coreconcept moved to components/CoreConcept.jsx
+// coreconcepts from static to dinamic rendering using props check imports and data.js
+// optized by mapping over array but here is manual for clarity
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts"> 
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
+
       </main>
     </div>
   );
