50e62fc63af16b00a2bb06c4f011e72f5d65dbe1
MiniMati <matiasefracchia@hottmail.com>
2026-01-06 15:08:20 -0300

    06012026 - Dynamic style applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
-
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data'; 
-
+import { EXAMPLES } from './data';
 
 
 
@@ -13,6 +14,26 @@ import { CORE_CONCEPTS } from './data';
 // coreconcepts from static to dinamic rendering using props check imports and data.js
 // optized by mapping over array but here is manual for clarity
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  let tabContent = <p>Please select a topic.</p>;
+
+
+  if (selectedTopic) {
+    tabContent = (<div id="tab-content">
+    <h3>{EXAMPLES[selectedTopic].title}</h3>
+    <p>{EXAMPLES[selectedTopic].description}</p>
+    <pre>
+      <code> {EXAMPLES[selectedTopic].code}</code>
+    </pre>
+    </div> 
+          );
+        }
+  
+  function handSelect(selectedButton){
+    setSelectedTopic(selectedButton);
+    //console.log(selectedButton);
+  } 
+
   return (
     <div>
       <Header />
@@ -25,8 +46,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
-
+        <section id= "examples">
+          <h2>Menu</h2>
+          <menu>
+            <TabButton label = 'Components' onSelect= {() => handSelect('components')}></TabButton>
+            <TabButton label = 'JSX' onSelect= {() => handSelect('jsx')}></TabButton>
+            <TabButton label = 'Props' onSelect= {() => handSelect('props')}></TabButton>
+            <TabButton label = 'State' onSelect= {() => handSelect('state')}></TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+export default function TabButton({label, onSelect}){
+    
+    return( 
+    <li>
+        <button onClick={onSelect}>{label}</button>
+    </li>
+        
+    );
+}
